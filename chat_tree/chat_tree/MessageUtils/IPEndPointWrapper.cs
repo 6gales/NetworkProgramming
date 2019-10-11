f0aded8855892519d8cbf01c757c9f3053fe0adb
6gales <andrey@xgaminggroup.com>
2019-10-11 10:05:50 +0700

    add ip endpoint wrapper

--- chat_tree/chat_tree/MessageUtils/IPEndPointWrapper.cs
+++ chat_tree/chat_tree/MessageUtils/IPEndPointWrapper.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ChatTree.MessageUtils
 {
+	[Serializable]
 	class IPEndPointWrapper
 	{
+		private string _host;
+		private int _port;
+
+		[NonSerialized]
+		private IPEndPoint _endPoint = null;
+
+		public IPEndPointWrapper(IPEndPoint endPoint)
+		{
+			_host = endPoint.Address.ToString();
+			_port = endPoint.Port;
+		}
+
+		public IPEndPointWrapper(string host, int port)
+		{
+			_host = host;
+			_port = port;
+		}
+
+		public IPEndPoint GetIPEndPoint()
+		{
+			if (_endPoint == null)
+				_endPoint = new IPEndPoint(IPAddress.Parse(_host), _port);
+			return _endPoint;
+		}
 	}
 }
