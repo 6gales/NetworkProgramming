c6abf4fe06b4f608f9cec76efa3dba3263873525
6gales <andrey@xgaminggroup.com>
2019-11-15 02:27:48 +0700

    make delete more effective

---
+++ WebSocketsChat/WebSocketsChat/ModelDefinition/IIdentifiedResource.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+namespace WebSocketsChat.ModelDefinition
+{
+	interface IIdentifiedResource
+	{
+		[JsonProperty("id")]
+		int Id { get; set; }
+	}
+
+	class IdentifiedResource : IIdentifiedResource
+	{
+		[JsonProperty("id")]
+		public int Id { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			if (obj is IIdentifiedResource idr)
+			{
+				return Id == idr.Id;
+			}
+
+			return false;
+		}
+
+		public override int GetHashCode()
+		{
+			return Id;
+		}
+	}
+}
